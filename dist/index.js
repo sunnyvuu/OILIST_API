df2271fdee1d2ec83fbb5091a558d6959a9410b9
Madeline Yi <sunhunfunbun@gmail.com>
2024-04-27 22:51:07 -0500

    Added api call to index

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,12 @@
 import express from "express";
+import tropeRoutes from "./routes/tropeRoutes.js";
 const app = express();
 const port = 3000;
+app.use(express.json());
 app.get("/", (req, res) => {
     res.send("Hello World with TypeScript!");
 });
+app.use("/api", tropeRoutes);
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import express from "express";
+import tropeRoutes from "./routes/tropeRoutes.js";
 
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello World with TypeScript!");
 });
+app.use("/api", tropeRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);

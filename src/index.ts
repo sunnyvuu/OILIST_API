fadddb87d8fd28ef0534d797a9606404477e2000
Madeline Yi <sunhunfunbun@gmail.com>
2024-05-15 21:18:33 -0500

    added manhwaRoutes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import tropeRoutes from "./routes/tropeRoutes.js";
 import cors from "cors";
+import manhwaRoutes from "./routes/manhwaRoutes.js";
 
 const app = express();
 const port = 3000;
@@ -11,7 +12,9 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello World with TypeScript!");
 });
+
 app.use("/api", tropeRoutes);
+app.use("/api", manhwaRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
